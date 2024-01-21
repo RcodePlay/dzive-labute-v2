d537b88bb96a0f06f4d20a61ba3835abd08f7076
RcodePlay <ryancode.eh@gmail.com>
2024-01-21 16:05:39 +0100

    Homepage updates, styles, and preparing for additional features

--- dzive-labute-web/ngApp/src/app/subpage-content/subpage-content.component.ts
+++ dzive-labute-web/ngApp/src/app/subpage-content/subpage-content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ArticlesService } from '../services/articles/articles.service';
 
 @Component({
   selector: 'app-subpage-content',
@@ -10,13 +11,24 @@ export class SubpageContentComponent {
   isHome: boolean = false
   isContact: boolean = false
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private articleService: ArticlesService) {}
 
   ngOnInit(): void {
     this.route.url.subscribe((segments) => {
       this.isHome = segments[0]?.path === 'home';
       this.isContact = segments[0]?.path === 'contact';
     });
+    if (this.isHome) {
+      this.getArticle()
+    }
+  }
+
+  articles: any
+
+  getArticle() {
+    this.articleService.getArticles().subscribe((articles) => {
+      this.articles = articles
+    })
   }
 
 
